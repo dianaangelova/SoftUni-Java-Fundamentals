e732f9ab5a1847ce4f499bb48e71816bb30fba51
Diana Angelova <diana.anguelova@gmail.com>
2022-06-30 23:41:49 +0300

    Add files via upload

---
+++ 04PF_Methods/Exercise/P07_NxNMatrix.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class P07_NxNMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        printMatrix(number);
+    }
+
+    private static void printMatrix(int number) {
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+    }
+}
