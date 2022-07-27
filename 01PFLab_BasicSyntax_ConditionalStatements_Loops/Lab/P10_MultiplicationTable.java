fbc33f960af75c0983120c39906263127671edb1
Diana Angelova <diana.anguelova@gmail.com>
2022-07-27 22:45:13 +0300

    Add files via upload

---
+++ 01PFLab_BasicSyntax_ConditionalStatements_Loops/Lab/P10_MultiplicationTable.java
@@ -0,0 +1,15 @@
+package Lab01_BasicSyntaxConditionalStatementsLoops;
+
+import java.util.Scanner;
+
+public class P10_MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= 10; i++) {
+            int product = number * i;
+            System.out.printf("%d X %d = %d%n", number, i, product);
+        }
+    }
+}
