903801eafca709be5f1f82f1d4eceeb147764fee
Diana Angelova <diana.anguelova@gmail.com>
2022-07-28 23:30:54 +0300

    Add files via upload

---
+++ 08PF_AssociativeArraysLambdaStreamAPI/Lab/P04_WordFilter.java
@@ -0,0 +1,19 @@
+package Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04_WordFilter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] wordsArr= Arrays
+                .stream(scanner.nextLine().split(" "))
+                .filter(w->w.length()%2==0)
+                .toArray(String[]::new);
+
+        System.out.println(String.join(System.lineSeparator(), wordsArr));
+
+    }
+}
