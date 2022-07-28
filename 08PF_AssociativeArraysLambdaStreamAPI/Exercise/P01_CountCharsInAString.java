2399cdce66847416d033395a30d39b9edcb4000e
Diana Angelova <diana.anguelova@gmail.com>
2022-07-28 23:31:58 +0300

    Add files via upload

---
+++ 08PF_AssociativeArraysLambdaStreamAPI/Exercise/P01_CountCharsInAString.java
@@ -0,0 +1,30 @@
+package Exsercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class P01_CountCharsInAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine().replaceAll("\\s+", "");
+        int count;
+
+        Map<Character, Integer> charNumberMap = new LinkedHashMap<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+            if (!charNumberMap.containsKey(character)) {
+                charNumberMap.put(character, 1);
+            } else {
+                count = charNumberMap.get(character);
+                charNumberMap.put(character, count + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : charNumberMap.entrySet()) {
+            System.out.printf("%s -> %d%n", entry.getKey(), entry.getValue());
+        }
+    }
+}
