0eb67b1b88ef90a7f557d121e3cdeb3a00bc079d
Diana Angelova <diana.anguelova@gmail.com>
2022-07-29 23:34:24 +0300

    Add files via upload

---
+++ 08PF_AssociativeArraysLambdaStreamAPI/Exercise/P06_Courses.java
@@ -0,0 +1,32 @@
+package Exsercise;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class P06_Courses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Map<String, List<String>> classMap = new LinkedHashMap<>();
+        while (!input.equals("end")) {
+            List<String> inputList = Arrays.stream(input.split(" : ")).collect(Collectors.toList());
+            String className = inputList.get(0);
+            String studentName = inputList.get(1);
+
+            if (!classMap.containsKey(className)) {
+                classMap.put(className, new ArrayList<String>());
+            }
+            classMap.get(className).add(studentName);
+
+            input = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, List<String>> c : classMap.entrySet()) {
+            int sizeList = c.getValue().size();
+            System.out.println(c.getKey() + ": " + sizeList);
+            for (String s : c.getValue()) {
+                System.out.println("-- " + s);
+            }
+        }
+    }
+}
