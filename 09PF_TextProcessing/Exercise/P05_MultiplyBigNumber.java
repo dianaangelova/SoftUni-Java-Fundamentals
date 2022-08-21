7b19fbea4329f0b20d1f050337a8d91dd5df7bd9
Diana Angelova <diana.anguelova@gmail.com>
2022-08-21 23:00:00 +0300

    Add files via upload

---
+++ 09PF_TextProcessing/Exercise/P05_MultiplyBigNumber.java
@@ -0,0 +1,29 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class P05_MultiplyBigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String number = scanner.nextLine();
+        int multiplier = Integer.parseInt(scanner.nextLine());
+        StringBuilder sb = new StringBuilder();
+        int reminder = 0;
+        for (int i = number.length() - 1; i >= 0; i--) {
+            int digit = Integer.parseInt(String.valueOf(number.charAt(i)));
+            int product = digit * multiplier + reminder;
+            if (i == 0) {
+                sb.insert(0, product);
+            } else {
+                int digitToAdd = product % 10;
+                reminder = product / 10;
+                sb.insert(0, digitToAdd);
+            }
+        }
+        while(sb.charAt(0)=='0' && sb.length()>1){
+            sb.deleteCharAt(0);
+        }
+        System.out.println(sb);
+    }
+}
